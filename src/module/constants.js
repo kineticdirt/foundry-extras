28d4922fa29d72b72c9277193ce641d158937cb6
kineticdirt <kineticdirt@gmail.com>
2025-11-22 16:27:22 -0800

    Fix MODULE_NAME constant to match module ID

--- src/module/constants.js
+++ src/module/constants.js
@@ -1,6 +1,6 @@
 const CONSTANTS = {
-  MODULE_NAME: 'playlist_import',
-  PATH: `modules/playlist_import/`,
+	MODULE_NAME: 'foundry-extra-lewd',
+	PATH: `modules/foundry-extra-lewd/`,
 };
 CONSTANTS.PATH = `modules/${CONSTANTS.MODULE_NAME}/`;
 export default CONSTANTS;
